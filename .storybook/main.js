c64e9ea48b3dd76abde372b6f0d33cdd0e88625d
Mahdi Rashidi <mr72302@gmail.com>
2025-04-13 21:40:59 +0330

    hotfix fix bugs in storybook configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,16 @@
+
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  staticDirs: ['../public'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
-  ],
-  framework: {
-    name: '@storybook/vue3-vite',
-    options: {}
-  }
-}
-export default config
+  stories: ['../src/components/**/*.stories.js'],
+   staticDirs: ['../public'],
+   addons: [
+     '@storybook/addon-links',
+     '@storybook/addon-essentials',
+     '@storybook/addon-interactions',
+   ],
+   framework: {
+     name: '@storybook/vue3-vite',
+     options: {},
+   },
+ };
+ export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,17 @@
+
+import '../src/index.css';
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/
-      }
-    }
-  }
-}
+        date: /Date$/,
+      },
+    },
+  },
+};
 
-export default preview
+export default preview;
