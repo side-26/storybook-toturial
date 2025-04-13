fc06e1fdd6ce35112c58b568bd4c0cb33c1d79db
Mahdi Rashidi <mr72302@gmail.com>
2025-04-13 21:39:13 +0330

    fix create test component called task and its story

---
+++ src/components/app/task/default.stories.js
@@ -0,0 +1,48 @@
+
+import { fn } from '@storybook/test';
+
+import AppTask from './default.vue';
+
+export const ActionsData = {
+  onPinTask: fn(),
+  onArchiveTask: fn(),
+};
+
+export default {
+  component: AppTask,
+  title: 'app-task',
+  tags: ['autodocs'],
+  //👇 Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/,
+  args: {
+    ...ActionsData
+  }
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: '1',
+      title: 'Test Task',
+      state: 'TASK_INBOX',
+    },
+  },
+};
+
+export const Pinned = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_PINNED',
+    },
+  },
+};
+
+export const Archived = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_ARCHIVED',
+    },
+  },
+};
